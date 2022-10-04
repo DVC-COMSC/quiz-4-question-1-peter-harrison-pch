4a37b1f98e39e1c8f7a997b561eb110119bcaced
Peter Harrison <peterharrison.pch@gmail.com>
2022-10-03 23:02:54 -0700

    first commit, first rough dragt

--- q1.py
+++ q1.py
@@ -1,8 +1,18 @@
+counter = 0
+remember = 0 
 
-# ******************************
-# Make your Code
-# ******************************
+for i in range(10):
+    num = int(input())
+    if num % 2 == 0:
+        if remember == 0:
+            remember = 1
+        elif remember == 1:
+            counter = counter + 1
+            remember = 2
+    elif num % 2 == 1:
+        remember = 0
 
+print(counter)
 
 # Requirement
 # No need to use list
